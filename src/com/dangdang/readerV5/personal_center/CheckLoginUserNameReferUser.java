e3892cd70af30256c5680c89da482b12749fc4bb
cailj <cailianjie@dangdang.com>
2015-07-02 15:22:59 +0800

    提交 设置新密码接口

---
+++ src/com/dangdang/readerV5/personal_center/CheckLoginUserNameReferUser.java
@@ -0,0 +1,11 @@
+package com.dangdang.readerV5.personal_center;
+
+import com.dangdang.autotest.common.FixtureBase;
+
+/**
+ * Created by cailianjie on 2015-6-29.
+ */
+public class CheckLoginUserNameReferUser extends FixtureBase{
+
+    public CheckLoginUserNameReferUser(){addAction("checkLoginUserNameReferUser");}
+}
