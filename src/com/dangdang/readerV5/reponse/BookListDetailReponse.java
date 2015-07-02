b638a6c44c0e69603ca3e930876631630f751f94
guohaiying <guohaiying@dangdang.com>
2015-07-02 10:34:05 +0800

    修改BookListDetailResponse

--- src/com/dangdang/readerV5/reponse/BookListDetailReponse.java
+++ src/com/dangdang/readerV5/reponse/BookListDetailReponse.java
@@ -3,27 +3,14 @@ package com.dangdang.readerV5.reponse;
 import java.util.List;
 
 public class BookListDetailReponse {
-	String currentDate;
 	List<MediaList> mediaList;
-	String systemDate;
 	Integer total;
-	public String getCurrentDate() {
-		return currentDate;
-	}
-	public void setCurrentDate(String currentDate) {
-		this.currentDate = currentDate;
-	}
+
 	public List<MediaList> getMediaList() {
 		return mediaList;
 	}
 	public void setMediaList(List<MediaList> mediaList) {
 		this.mediaList = mediaList;
-	}
-	public String getSystemDate() {
-		return systemDate;
-	}
-	public void setSystemDate(String systemDate) {
-		this.systemDate = systemDate;
 	}
 	public Integer getTotal() {
 		return total;
