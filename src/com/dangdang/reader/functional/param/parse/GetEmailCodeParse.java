750b9bd25af1f7026f93c1a239c69a07488451b3
cailj <cailianjie@dangdang.com>
2015-07-03 16:34:42 +0800

    个人中心新增接口脚本

---
+++ src/com/dangdang/reader/functional/param/parse/GetEmailCodeParse.java
@@ -0,0 +1,7 @@
+package com.dangdang.reader.functional.param.parse;
+
+/**
+ * Created by cailianjie on 2015-7-2.
+ */
+public class GetEmailCodeParse {
+}
---
+++ src/com/dangdang/readerV5/reponse/DdMoney.java
@@ -0,0 +1,7 @@
+package com.dangdang.readerV5.reponse;
+
+/**
+ * Created by cailianjie on 2015-7-2.
+ */
+public class DdMoney {
+}
