f46d1f422c123e4eb8614ed2cb8c9e4a0e67fa2b
cailj <cailianjie@dangdang.com>
2015-07-08 19:18:55 +0800

    漏提交

--- src/com/dangdang/autotest/common/PlatForm.java
+++ src/com/dangdang/autotest/common/PlatForm.java
@@ -9,8 +9,10 @@ public enum PlatForm {
 	DDREADER_ANDROID("ds_android"),//当当读书安卓
 	DDREADER_IOS("ds_ios"),//当当读书ios
 	YC_ANDROID("yc_android"),//原创安卓
-	YC_IOS("yc_ios");//原创ios
-	
+	YC_IOS("yc_ios"),//原创ios
+	DS("ds"),//当当读书平台
+	YC("yc");//当读小说平台
+
 	
 	 private String context;
 		
--- src/com/dangdang/digital/MediaDb.java
+++ src/com/dangdang/digital/MediaDb.java
@@ -11,9 +11,20 @@ public class MediaDb {
     /*
        获取一个有效的mediaId
     */
-    public  static String getMediaId(BookType bookType) throws Exception {
-        String selectString="select * from media where "+(bookType==BookType.EBOOK?"doc_type='ebook'":"doc_type is null")+" and shelf_status =1 limit 1";
+    public  static String getMediaId(BookType bookType,BookStatus bookStatus) throws Exception {
+        String shelfStatus=bookStatus==BookStatus.VALID?"1":"0";
+        String selectString="select m.* from media m left join media_sale ms on m.sale_id=ms.sale_id where ms.shelf_status="+shelfStatus+" and m.shelf_status="+shelfStatus+" and "+(bookType==BookType.EBOOK?"doc_type='ebook'":"doc_type is null")+" limit 1";
+        Media media = DbUtil.selectOne(com.dangdang.config.Config.YCDBConfig, selectString, Media.class);
+        return media.getMediaId().toString();
+    }
 
+    /*
+    获取免费书籍Id
+     */
+    public  static String getFreeMediaId(BookType bookType,BookStatus bookStatus) throws Exception {
+        String shelfStatus=bookStatus==BookStatus.VALID?"1":"0";
+        //String selectString="select m.* from media m left join media_sale ms on m.sale_id=ms.sale_id where ms.price=0 and ms.shelf_status="+shelfStatus+" and m.shelf_status="+shelfStatus+" and "+(bookType==BookType.EBOOK?"doc_type='ebook'":"doc_type is null")+" limit 1";
+        String selectString="select * from media_activity_sale mas left join media m on m.sale_id=mas.sale_id where m.shelf_status="+bookStatus+" and mas.sale_price=0" +" and "+(bookType==BookType.EBOOK?"doc_type='ebook'":"doc_type is null")+" limit 1";
         Media media = DbUtil.selectOne(com.dangdang.config.Config.YCDBConfig, selectString, Media.class);
         return media.getMediaId().toString();
     }
